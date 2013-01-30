6c43504e2f5edd9644b7aad9c704081742335d8f
Marc von Holzen <marc@vonholzen.org>
2013-01-30 12:15:34 -0800

    Check in utility method getParameterByName.  Should probably be in a different file.

--- notepad/notepad/rdf.js
+++ notepad/notepad/rdf.js
@@ -2,6 +2,17 @@
 
     $.notepad = $.notepad || {};
 
+    $.notepad.getParameterByName = function(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.search);
+        if(results == null)
+            return "";
+        else
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
     $.notepad.uri = function() {
         return $.uri.base().toString().replace(/#.*$/,'');
     }
