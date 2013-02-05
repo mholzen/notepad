ce4a531ad8d6c367e52b1ea744038cf87ddd77fd
Marc von Holzen <marc@vonholzen.org>
2013-02-05 12:12:34 -0800

    Move code at the end of the file.

--- notepad/notepad/endpoint.js
+++ notepad/notepad/endpoint.js
@@ -1,50 +1,5 @@
 (function($, undefined) {
 
-    ContainerChainEndpoint = function (container) {
-        this.container = container;
-    }
-    ContainerChainEndpoint.prototype = {
-        execute: function(command, callback) {
-
-            var results = this.container.triples().execute(command, function(results) {
-                if (results.length != 0) {
-                    callback(results);
-                }
-            });
-
-            // next endpoint
-            var parentElement = this.container.element.parent();
-            if (parentElement.length === 0) {
-                log.warn('cannot find parent element to container');
-                return;
-            }
-            var parentEndpoint = parentElement.findEndpoint();
-            if (parentEndpoint === undefined) {
-                log.warn('cannot find parent endpoint');
-                return;
-            }
-
-            return parentEndpoint.execute(command, callback);
-        }
-    };
-
-    ChainEndpoint = function (firstEndpoint, nextEndpoint) {
-        this.firstEndpoint = firstEndpoint;
-        this.nextEndpoint = nextEndpoint;
-    }
-    ChainEndpoint.prototype = {
-        execute: function(command, callback) {
-            var results = this.firstEndpoint.execute(command);
-            if (results.length == 0) {
-                return this.nextEndpoint.execute(command, callback);
-            }
-            if (callback) {
-                callback(results);
-            }
-            return results;
-        }
-    };
-
     $.widget("notepad.endpoint", {
 
         // This widget manages the "notepad:endpoint" predicate under its DOM element
@@ -150,5 +105,50 @@
         }
         return element.data('notepadEndpoint').getEndpoint();
     }
+
+    ContainerChainEndpoint = function (container) {
+        this.container = container;
+    }
+    ContainerChainEndpoint.prototype = {
+        execute: function(command, callback) {
+
+            var results = this.container.triples().execute(command, function(results) {
+                if (results.length != 0) {
+                    callback(results);
+                }
+            });
+
+            // next endpoint
+            var parentElement = this.container.element.parent();
+            if (parentElement.length === 0) {
+                log.warn('cannot find parent element to container');
+                return;
+            }
+            var parentEndpoint = parentElement.findEndpoint();
+            if (parentEndpoint === undefined) {
+                log.warn('cannot find parent endpoint');
+                return;
+            }
+
+            return parentEndpoint.execute(command, callback);
+        }
+    };
+
+    ChainEndpoint = function (firstEndpoint, nextEndpoint) {
+        this.firstEndpoint = firstEndpoint;
+        this.nextEndpoint = nextEndpoint;
+    }
+    ChainEndpoint.prototype = {
+        execute: function(command, callback) {
+            var results = this.firstEndpoint.execute(command);
+            if (results.length == 0) {
+                return this.nextEndpoint.execute(command, callback);
+            }
+            if (callback) {
+                callback(results);
+            }
+            return results;
+        }
+    };
 
 }(jQuery));
