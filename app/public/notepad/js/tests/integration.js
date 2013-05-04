57edf0994f8e8d80b11f25db00e95e85f9863762
Marc von Holzen <marc@vonholzen.org>
2013-05-04 15:49:07 -0700

    refactor to jquery style document ready.

--- app/public/notepad/js/tests/integration.js
+++ app/public/notepad/js/tests/integration.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+(function($, undefined) {
+
+	// For tests, set the empty prefix to the Dev point
+	$.notepad.namespaces[''] = 'http://localhost:3030/dev#';
+
     JsMockito.Integration.QUnit();
     JsHamcrest.Integration.QUnit(); 
-});
+
+}(jQuery));
